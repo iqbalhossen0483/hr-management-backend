d87812fbd4af7a74f7d072f9a9a6d13b73132259
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-02-08 14:37:29 +0600

    static file added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './config/database.module';
 import { ENVConfigModule } from './config/env-config.module';
 import { JWTConfigModule } from './config/jwt.config.module';
+import { StaticModule } from './config/static.module';
 import { EmployeeModule } from './modules/employee/employee.module';
 import { HrUserModule } from './modules/hrUser/hrUser.module';
 
@@ -10,6 +11,7 @@ import { HrUserModule } from './modules/hrUser/hrUser.module';
     ENVConfigModule,
     DatabaseModule,
     JWTConfigModule,
+    StaticModule,
     HrUserModule,
     EmployeeModule,
   ],
